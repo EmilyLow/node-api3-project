f78aebf746bbcf795a89a1f13f221e925379542b
EmilyLow <emlow@me.com>
2020-10-08 12:36:11 -0500

    Added .env

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 // code away!
+require('dotenv').config();
 const server = require("./server.js");
 
-const port = 4000;
-server.listen(4000, () => {
-  console.log("Server listening on port 4000...");
+const port = process.env.PORT || 5000;
+
+
+server.listen(port, () => {
+  console.log("Server listening on port 5000...");
 });
 
 //Why do we have both index.js and server.js here? Is it because you can only put the location in index.js?
